1e6793729648f302e1edbe9480d68f8bbb639a48
RiskalaTed <riskalatedoldi@gmail.com>
2025-05-15 03:20:49 -0300

    Adiciona rota POST

--- src/app.jl
+++ src/app.jl
@@ -1,18 +1,25 @@
-
 include("../routes/login.jl")
+include("../routes/data.jl")
 
 using HTTP
 using Logging
 using .LoginRoute
-
+using .DataRoute
 
 function main()
+    # Inicializa ambos os CSVs (usuários e dados de mosquitos)
     init_users_csv()
+    init_data_csv()
+
     println("Starting server on http://127.0.0.1:8080...")
 
     HTTP.serve(request -> begin
         if request.method == "POST" && request.target == "/login"
             return login_handler(request)
+
+        elseif request.method == "POST" && request.target == "/mosquito-data"
+            return data_handler(request)
+
         else
             return HTTP.Response(404, "Not Found")
         end
